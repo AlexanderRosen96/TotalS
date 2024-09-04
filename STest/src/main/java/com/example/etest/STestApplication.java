5df1f7753df012c4df1205b494d761f59b40c41f
Rosen <Alexander.rosen.JAVA23@edu.edugrade.se>
2024-09-04 16:21:27 +0200

    Test2

--- STest/src/main/java/com/example/etest/STestApplication.java
+++ STest/src/main/java/com/example/etest/STestApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ETestApplication {
+public class STestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ETestApplication.class, args);
+        SpringApplication.run(STestApplication.class, args);
     }
 
 }
