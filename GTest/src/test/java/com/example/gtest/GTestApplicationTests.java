6f1211bd381a9b5948e3d222ca7f92b19ddd4f3f
Rosen <Alexander.rosen.JAVA23@edu.edugrade.se>
2024-09-04 14:44:27 +0200

    New

---
+++ GTest/src/test/java/com/example/gtest/GTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
